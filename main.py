a571de83adc5226f8dbf2652158278be3f8e273b
Tiago Souza Paiva <118641977+MrPaivas@users.noreply.github.com>
2023-03-17 21:51:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,15 +9,26 @@ agenda = "./arquivos/agenda.xlsx"
 df = pd.read_excel(agenda)
 salva_hora = []
 
+
+def manda_o_zap(tel, nome, hora, local, paciente):
+    pywhatkit.sendwhatmsg_to_group_instantly(
+                                            tel, f'Oi {nome}, foi marcado com sucesso na '
+                                            f'{hora} no {local} paciente {paciente}')
+
+
 for index, row in df.iterrows():
     dia_consultorio = row['SEGUNDA']
     nome_paciente = row['Unnamed: 1']
     nome_estagiario = row['Unnamed: 3']
     telefone_estagiario = row['Unnamed: 4']
 
-    if dia_consultorio in segunda:
+    if nome_estagiario == 'ESTAGIÁRIO':
         salva_hora.append(dia_consultorio)
+        continue
+    elif nome_paciente == "":
+        continue
+    print(index, dia_consultorio, nome_paciente, nome_estagiario, telefone_estagiario)
+    #else:
+        #manda_o_zap(telefone_estagiario, nome_estagiario, salva_hora[-1], dia_consultorio, nome_paciente)
 
-    pywhatkit.sendwhatmsg_to_group_instantly(telefone_estagiario, f'Oi {nome_estagiario}, foi marcado com sucesso na '
-                                                                  f'{salva_hora} no {dia_consultorio} paciente '
-                                                                  f'{nome_paciente}')
+
